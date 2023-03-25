2aa7d237bc86062417858d0db6cb3f48811f3bd2
Max Pieshkov <49279392+soulsword1@users.noreply.github.com>
2023-03-25 13:51:17 +0200

    final homework changes

---
+++ src/index.js
@@ -0,0 +1,86 @@
+import './css/styles.css';
+import fetchCountries from './js/fetchCountries';
+import Debounce from 'lodash.debounce';
+import { Notify } from 'notiflix';
+
+const DEBOUNCE_DELAY = 300;
+
+const countryInput = document.querySelector('#search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
+
+countryInput.addEventListener(
+  'input',
+  Debounce(() => addingCountriesToHTML(), DEBOUNCE_DELAY)
+);
+
+function addingCountriesToHTML() {
+  const name = countryInput.value.trim();
+  if (name) {
+    fetchCountries(name)
+      .then(countries => {
+        const countriesQty = countries.length;
+        const min = 2;
+        const max = 10;
+        if (countriesQty > max) {
+          tooManyNotification();
+          return;
+        } else if (countriesQty > min && countriesQty < max) {
+          showCountryList(countries);
+          countryInfo.innerHTML = '';
+          return;
+        }
+        showInformationAboutCountry(countries);
+      })
+      .catch(error => {
+        notFoundNotification();
+        console.log(`Error ${error}`);
+      });
+  }
+}
+
+function tooManyNotification() {
+  clearHTML();
+  Notify.info('Too many matches found. Please enter a more specific name.');
+}
+
+function notFoundNotification() {
+  clearHTML();
+  Notify.failure('Oops, there is no country with that name');
+}
+
+function clearHTML() {
+  countryList.innerHTML = '';
+  countryInfo.innerHTML = '';
+}
+
+function showCountryList(countries) {
+  const markup = countries
+    .map(
+      ({ name, flags }) =>
+        `<li class="country-item"><img src='${flags.svg}' width='100px' height='75px'/><span class="country-item__name">${name.common}</span></li>`
+    )
+    .join('');
+  countryList.innerHTML = markup;
+}
+
+function showInformationAboutCountry(countries) {
+  showCountryList(countries);
+  document.querySelector('.country-item__name').classList.add('bigger-font');
+  document
+    .querySelector('.country-item img')
+    .classList.add('country-item__img');
+  const markup2 = countries
+    .map(({ capital, population, flags, languages }) => {
+      const countryLanguages = Object.values(languages);
+      return `<div class="container-details"><p><span class="bold">Capital:</span> ${capital}</p>
+            <p><span class="bold">Population:</span> ${population}</p>
+            <p><span class="bold">Languages:</span> ${countryLanguages.map(
+              lang => `${lang}`
+            )}</p>
+            </div>
+            `;
+    })
+    .join('');
+  countryInfo.innerHTML = markup2;
+}
---
+++ src/js/fetchCountries.js
@@ -0,0 +1,11 @@
+export default function fetchCountries(name) {
+    // const name = countryInput.value;
+    const url = `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`;
+    return fetch(url)
+        .then(response =>{
+            if(!response.ok){
+                throw new Error(response.status);;
+            }
+            return response.json();
+    });
+}
