2ea3fde3f29513ebc9ca30448d5adbe7a0481066
Max Pieshkov <49279392+soulsword1@users.noreply.github.com>
2023-04-03 13:09:04 +0300

    checking empty search field

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -7,3 +7,13 @@ async function fetchCountries(name) {
     return fetchResult;
 }
 
+// async function fetchCountries(name) {
+// try {
+//     const url = `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`;
+//     const fetchUrl = await fetch(url);
+//     const fetchResult = await fetchUrl.json();
+//     return fetchResult;
+// } catch(e) {
+//     console.log('error: ', e);  
+// }
+// }
